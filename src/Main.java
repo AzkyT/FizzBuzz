1ed4d49272291343592e001946adf43ff05f2d18
AzkyT <alextran.quandinh@gmail.com>
2023-09-11 14:29:24 -0400

    main file that runs similar to FizzBuzz.main

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int i = 0;
+        while (i < 100) {
+            FizzBuzz.doFizzBuzz(i);
+            i++;
+        }
+    }
+}
+
